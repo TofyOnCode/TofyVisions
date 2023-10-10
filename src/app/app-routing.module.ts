377c3388fc5e08b374b46a7512c30039e70757cc
TofyOnCode <tofant.timon1@gmail.com>
2023-10-10 19:13:50 +0200

    drugi commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  // Dodajte ostale poti tukaj
+  { path: '', redirectTo: '/login', pathMatch: 'full' }  // Privzeta pot, ki preusmeri na stran za prijavo
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
